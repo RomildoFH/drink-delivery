0f2b2351e20845a53f94156f76edba1f6dc0e276
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-05-19 18:24:36 -0300

    deploy: fixing initial state for the login form

--- front-end/src/context/AppProvider.js
+++ front-end/src/context/AppProvider.js
@@ -9,8 +9,8 @@ function AppProvider({ children }) {
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useLocalStorage('user', {});
   const [fields, setFormFields] = useForm({
-    email: '',
-    password: '',
+    email: 'zebirita@email.com',
+    password: '$#zebirita#$',
     name: '',
     role: '',
   });
